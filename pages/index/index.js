5da1d44e9a55c92931b98b360d20f583ee0acac0
alluserbegin <597153059@qq.com>
2020-09-17 17:34:41 +0800

    1

--- pages/index/index.js
+++ pages/index/index.js
@@ -11,6 +11,19 @@ Page({
       'http://wuwuditu.img-cn-hangzhou.aliyuncs.com/cms/20200915/f5ffbcd08cd0468197f3bc593dec0fa9.jpg',
       'http://wuwuditu.img-cn-hangzhou.aliyuncs.com/cms/20200915/46c1653d986c43b283d6ccbe270e360e.jpg'
     ],
+    //优质商家推荐
+    topUrls:[
+      'http://wuwuditu.img-cn-hangzhou.aliyuncs.com/cms/20200915/374cc11dbff54e98bd8f8d3406eb8682.jpg',
+      'http://wuwuditu.img-cn-hangzhou.aliyuncs.com/cms/20200915/7496fab158e24a728de9baae80501225.jpg' 
+    ],
+     //附近商家
+     nearUrls:[
+      'http://wuwuditu.img-cn-hangzhou.aliyuncs.com/cms/20200915/374cc11dbff54e98bd8f8d3406eb8682.jpg',
+      'http://wuwuditu.img-cn-hangzhou.aliyuncs.com/cms/20200915/7496fab158e24a728de9baae80501225.jpg',
+      'http://wuwuditu.img-cn-hangzhou.aliyuncs.com/cms/20200917/72ed5f6190654a75b3d60a7daa6356a1.jpg',
+      'http://wuwuditu.img-cn-hangzhou.aliyuncs.com/cms/20200917/20f166f2a287449d9cb54af857e2eed6.jpg',
+      'http://wuwuditu.img-cn-hangzhou.aliyuncs.com/cms/20200917/3f9347b068834916a9a47b765eb09fe3.jpg'
+    ],
     indicatorDots: true,
     autoplay: true,
     interval: 3000, //自动切换时间间隔
